aeca3721c4daae06c336e62b6e3beb485d46fabc
cxvh <lvcaodi@outlook.com>
2021-04-17 01:41:48 +0800

    修复 hexo 不识别 {% {{ 导致报错的 bug

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 /* global hexo */
 'use strict';
-hexo.extend.console.register('reader', '使用订阅器或者克隆整站文章', (options, callback) => require("./lib")(hexo, options, callback))
+hexo.extend.console.register('reader', '使用订阅器或者克隆整站文章', (options, callback) => require('./lib')(hexo, options, callback));
